032b3d1fff068132cec6dc113b691711e71a3abc
Timothy Axford <t.axford2@yahoo.com>
2022-12-04 13:23:45 -0500

    Added console code to change Forground color to blue.

--- HelloWorld.Net5/Program.cs
+++ HelloWorld.Net5/Program.cs
@@ -7,9 +7,12 @@ namespace HelloWorld.Net5
         // starting point of our program
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Tim!");
+
             Console.Read();
+            
         }
     }
 }
