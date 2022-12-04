1070792f2991857283af164b7935cc327e3aa0eb
Timothy Axford <t.axford2@yahoo.com>
2022-12-04 13:46:04 -0500

    Added examples of Implicit, Explicit, and Type conversions

--- HelloWorld.Net5/Program.cs
+++ HelloWorld.Net5/Program.cs
@@ -19,12 +19,24 @@ namespace HelloWorld.Net5
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Tim!");
 
+            // implicit conversion
+            int num = 12345678;
+            long bigNum = num;
+
+            float myFloat = 13.37f;
+            double myNewDouble = myFloat;
+            string myFloatString = myFloat.ToString();
 
             double myDouble = 13.37;
             int myInt;
-
+            // explicit conversion
             // cast double to int;
             myInt = (int)myDouble;
+
+            // typeConversion
+            string myString = myDouble.ToString(); // "13.37"
+            num.ToString();
+
             Console.WriteLine(myInt);
 
             Console.Read();
