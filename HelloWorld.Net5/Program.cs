8257b8ca3692dc28e98794e72396e0dd4664d96a
Timothy Axford <t.axford2@yahoo.com>
2022-12-04 13:20:48 -0500

    Add project files.

---
+++ HelloWorld.Net5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld.Net5
+{
+    internal class Program
+    {
+        // starting point of our program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Tim!");
+            Console.Read();
+        }
+    }
+}
