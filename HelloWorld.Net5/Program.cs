97548323e36485a9631691abc1cd268248cf9529
Timothy Axford <t.axford2@yahoo.com>
2022-12-04 13:33:50 -0500

    Added comments to explain proper formating

--- HelloWorld.Net5/Program.cs
+++ HelloWorld.Net5/Program.cs
@@ -2,13 +2,19 @@
 
 namespace HelloWorld.Net5
 {
+    // Class names like ClientActivity
     internal class Program
     {
         // starting point of our program
+        // Method name like CalculateValue
+        // method arguments like firstNumber
         static void Main(string[] args)
         {
+            //changes text color
             Console.ForegroundColor = ConsoleColor.Gray;
+            //changes background of text
             Console.BackgroundColor = ConsoleColor.Red;
+            //allows background change of entire console
             Console.Clear();
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Tim!");
